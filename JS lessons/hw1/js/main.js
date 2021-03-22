dda121bbb2038d1ac22fd7fb06b29f64afdff84f
YurgenZ <hgertsdorf@gmail.com>
2021-03-23 01:26:40 +0200

    hw js1

--- JS lessons/hw1/js/main.js
+++ JS lessons/hw1/js/main.js
@@ -1,1 +1,134 @@
-asd
+// let a = 'hello';
+// let b = 'owu';
+// let c = 'com';
+// let d = 'ua';
+// let e = 1;
+// let f = 10;
+// let g = -999;
+// let k = 123;
+// let l = 3.14;
+// let m = 2.7;
+// let n = 16;
+// let o = true;
+// let p = false;
+
+let a =1;
+let b =2;
+let c =3;
+let d =4;
+let e =5;
+let f ='try';
+let g ='CPU';
+let k ='work';
+let l =true;
+let m =false;
+let n ='true';
+let o =-56465456;
+let p ='=-=--=-=-=-=-=-=-=-=-=-=-=-';
+
+
+console.log (a);
+console.log (b);
+console.log (c);
+console.log (d);
+console.log (e);
+console.log (f);
+console.log (g);
+console.log (k);
+console.log (l);
+console.log (m);
+console.log (n);
+console.log (o);
+console.log (p);
+
+// alert(a);
+// alert(b);
+// alert(c);
+// alert(d);
+// alert(e);
+// alert(f);
+// alert(g);
+// alert(k);
+// alert(l);
+// alert(m);
+// alert(n);
+// alert(o);
+// alert(p);
+
+
+document.write(a);
+document.write(b);
+document.write(c);
+document.write(d);
+document.write(e);
+document.write(f);
+document.write(g);
+document.write(k);
+document.write(l);
+document.write(m);
+document.write(n);
+document.write(o);
+document.write(p);
+
+const a1 = 13;
+const a2 = -3;
+const a3 = 135252;
+const b1 = 'write';
+const b2 = 'this';
+const b3 = 'text';
+
+console.log (a1);
+console.log (a2);
+console.log (a3);
+console.log (b1);
+console.log (b2);
+console.log (b3);
+
+// alert (a1);
+// alert (a2);
+// alert (a3);
+// alert (b1);
+// alert (b2);
+// alert (b3);
+
+document.write(a1);
+document.write(a2);
+document.write(a3);
+document.write(b1);
+document.write(b2);
+document.write(b3);
+
+let xxx = prompt();
+// console.log(xxx);
+// document.write(xxx)
+alert(xxx)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
