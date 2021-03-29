26d048b6e74881d0e6daac0b7136e2fbbaf34f20
YurgenZ <hgertsdorf@gmail.com>
2021-03-29 19:48:18 +0300

    JS practice 2 in process

--- JS lessons/practice/practice_2/js/mainPractice_js2.js
+++ JS lessons/practice/practice_2/js/mainPractice_js2.js
@@ -273,32 +273,58 @@
 // }
 // console.log(arrRandomNum);
 
+//так якось зручніше
 
 // let arrRandomNum = [];
+
 //     for (let i = 0; i < 20; i++) {
-//         function getRandomInt(min, max) {
-//             min = Math.ceil(1);
-//             max = Math.floor(100);
-//             return Math.floor(Math.random() * (11 - 234)) + min;
-//
-//             console.log()
-//         }
-//             // console.log(arrRandomNum[i]);
+//        let r = parseInt(Math.random()*100;
+// arrRandomNum.push(r);
 // }
-
-// ????? треба придумать як це запхать у масив.
-
-
+// console.log(arrRandomNum);
 
 // d. Заповнити масив 20ма рандомними чисалами в діапазоні від 8 до 732 (Google: Generate random number JS)
-// 2. Вивести за допомогою console.log кожен третій елемен
+
+// let arrRandomNum = [];
+
+// for (let i = 0; i < 20; i++) {
+//     let r = parseInt(Math.random() * 732);
+//     if (r > 8) {
+//         arrRandomNum.push(r);
+//     }
+// }
+//     document.write(`${arrRandomNum}`+'');
+//     console.log(arrRandomNum);
+
+// 2. Вивести за допомогою console.log кожен третій елемент
+
+let arrRandomNum = [];
+
+for (let i = 0; i < 20; i++) {
+    let r = parseInt(Math.random() * 732);
+    arrRandomNum.push(r);
+    if (i % 3 === 0 && i > 0){
+        console.log(arrRandomNum[i])
+    }
+}
+console.log(arrRandomNum);
 // 3. Вивести за допомогою console.log кожен третій елемен тільки якщо цей елемент є парним.
-// 4. Вивести за допомогою console.log кожен третій елемен тільки якщо цей елемент є парним та записати їх в новий масив
+
+
+// 4. Вивести за допомогою console.log кожен третій елемен тільки якщо цей елемент є парним та записати їх
+// в новий масив
+
+
 // 5. Вивести кожен елемент масиву, сусід справа якого є парним
 // EXAMPLE: [ 1, 2, 3, 5, 7, 9, 56, 8, 67 ] -> Має бути виведено 1, 9, 56
+
+
 // 6. Є масив з числами [100,250,50,168,120,345,188], Які характеризують вартість окремої покупки. Обрахувати середній чек.
-//
+
+
 // 7. Створити масив з рандомними значеннями, помножити всі його елементи на 5 та перемістити їх в інший масив.
+
+
 // 8. Створити масив з будь якими значеннями (стрінги, числа, і тд...). пройтись по ньому, і якщо елемент є числом - додати його в інший масив.
 
 
