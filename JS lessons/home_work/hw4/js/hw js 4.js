216804c9891aa6e38e927046092401d3cf53b082
YurgenZ <hgertsdorf@gmail.com>
2021-04-10 01:40:19 +0300

    hw 4 sumSomeElement of arr

--- JS lessons/home_work/hw4/js/hw js 4.js
+++ JS lessons/home_work/hw4/js/hw js 4.js
@@ -1,13 +1,10 @@
 // 1) створити функцію яка приймає масив та виводить його
-
-function getResult(arrayInput) {
-
-    console.log(arrayInput);
-    // return arrayInput
-}
-
-/*let a = */
-getResult([1, 23, 5, 6, 8, 'sfd', 77]);
+// let arr = [1, 23, 5, 6, 8, 'sfd', 77];
+// function getResult(arrayInput) {
+//     console.log(arrayInput);
+// }
+//
+// let a = getResult(arr);
 // console.log(a);
 
 // 2) створити функцію яка заповнює масив рандомними числами та виводить його. Для виведення використати попередню функцію.
@@ -64,21 +61,27 @@ getResult([1, 23, 5, 6, 8, 'sfd', 77]);
 
 // 5) створити функцію яка повертає найбільше число з масиву
 
+// let arrNums = [1, 23, 5, 6, 8, 'sfd', 77];
+//
 // function maxArrNum(enter_arr = []) {
 //     let max = enter_arr[0];
 //
 //     for (let i = 1; i < enter_arr.length; i++) {
 //         if (enter_arr[i] > max) {
-//             max = enter_arr[i]
+//             max = enter_arr[i];
 //         }
 //     }
 //     return max
 // }
 //
-// let maxNums = maxArrNum([30, 24, 95, 52333, 43, 54, 36, 11, 87, 3])
+// let maxNums = maxArrNum(arrNums);
 // console.log(maxNums);
 
 // 6) створити функцію яка повертає найменьше число з масиву
+
+
+//  let arrNums = [1, 23, 5, 6, 8,-8, 'sfd', 77];
+//
 // function minArrNum(enter_arr = []) {
 //     let min = enter_arr[0];
 //     let length = enter_arr.length
@@ -89,13 +92,14 @@ getResult([1, 23, 5, 6, 8, 'sfd', 77]);
 //     }
 //     return min;
 // }
-// let minArrNums = minArrNum([0, 24, 95, 52333, 43, 54, 36, 11, 87, 3]);
+// let minArrNums = minArrNum(arrNums);
 // console.log(minArrNums);
-
 
 
 // 7) створити функцію яка приймає масив чисел, сумує значення елементів масиву та повертає його.
 
+//  let arrNums = [1, 23, 5, 6, 8,-8,  77];
+//
 // function sumSomeNums(enter_value = []) {
 //     let arrNum = enter_value;
 //
@@ -106,11 +110,13 @@ getResult([1, 23, 5, 6, 8, 'sfd', 77]);
 //     return sumElements
 // }
 //
-// let sumElement = sumSomeNums([1, 2, 3, 4])
+// let sumElement = sumSomeNums(arrNums)
 // console.log(sumElement);
 
 // 8) створити функцію яка приймає масив чисел та повертає середнє арифметичне його значень.
 
+// let arr = [1, 4, 3, 4];
+//
 // function arrMean(enter_value = []) {
 //     let arrNum = enter_value;
 //
@@ -121,7 +127,7 @@ getResult([1, 23, 5, 6, 8, 'sfd', 77]);
 //     return mean / arrNum.length
 // }
 //
-// let arrMeans = arrMean([1, 4, 3, 4])
+// let arrMeans = arrMean(arr);
 // console.log(arrMeans);
 
 
@@ -129,6 +135,9 @@ getResult([1, 23, 5, 6, 8, 'sfd', 77]);
 // EXAMPLE:
 //     [{name: 'Dima', age: 13}, {model: 'Camry'}]  ===> [ name, age, model ]
 
+// let arrOfObj = [{name: 'Dima', age: 13}, {model: 'Camry'},
+//     {name: 'Dima', age: 13}, {model: 'Camry'}];
+//
 // function returnKey(enter_array = 0) {
 //     let arrAll = [];
 //     for (const arrElement of enter_array) {
@@ -137,39 +146,56 @@ getResult([1, 23, 5, 6, 8, 'sfd', 77]);
 //     return arrAll.flat();
 // }
 //
-// arrFunc(returnKey([{name: 'Dima', age: 13}, {model: 'Camry'},
-//     {name: 'Dima', age: 13}, {model: 'Camry'}]))
+// let logger = returnKey(arrOfObj);
+// console.log(logger);
 
 // 10) Створити функцію яка приймає масив будь яких объектів, та повертає масив значень всіх обєктів
 // EXAMPLE:
 //     [{name: 'Dima', age: 13}, {model: 'Camry'}]  ===> [ Dima, 13, Camry ]
 
-// function arrValue(enter_arr = []) {
-//     let arrAll = [];
-//     for (const enterArrKey of enter_arr) {
-// if (enterArrKey  === enter_arr.value)
-//         console.log(enterArrKey);
-//     }
-//         arrAll.push(Object.values(enter_arr))
+// let arrOfValues = [{name: 'Dima', age: 13}, {model: 'Camry'},
+//     {name: 'ivan', age: 13}, {model: 'zaz'}];
 //
-// return arrAll.flat();
+// function arrValue(enter_array = []) {
+//     let arrAll = [];
+//     for (const arrElement of enter_array) {
+//         arrAll.push(Object.values(arrElement));
+//     }
+//     return arrAll.flat();
 // }
-
-// arrFunc(arrValue([{name: 'Dima', age: 13}, {model: 'Camry'},
-//     {name: 'Dima', age: 13}, {model: 'Camry'}]))
+//
+// let logger = arrValue(arrOfValues);
+// console.log(logger);
 
 // 11) створити функцію  яка скаладає значення елементів з однаковими індексами  та повертає новий результуючий масив.
 //     EXAMPLE:
 // [1,2,3,4]
-//     [2,3,4,5]
+// [2,3,4,5]
 // результат
-//     [3,5,7,9]
+// [3,5,7,9]
 //
 //
+
+let arrOfNums1 = [1, 2, 3, 4];
+let arrOfNums2 = [4, 3, 2, 1];
+
+function sumSomeElements(arr1, arr2) {
+    let sum = [];
+    for (const arr1Element of arr1) {
+        for (const arr2Element of arr2) {
+            if (arr1Element === arr2Element)
+                sum.push(arr1(arr1Element) + arr2(arr2Element));
+            console.log(arr2Element);
+        }
+    }
+    return sum
+}
+
+let sum = sumSomeElements(arrOfNums1, arrOfNums2);
+console.log(sum);
 // ============================================================================================
 //
 // - створити функцію яка приймає будь-яку кількість чисел, повертає найменьше, а виводить найбільше (Math використовувати заборонено);
-
 
 
 // - Взяти задачі з завдання 10 та 9 та обєднати їх в одну динамічну функцію.
@@ -225,21 +251,23 @@ getResult([1, 23, 5, 6, 8, 'sfd', 77]);
 // Створити функцію яка :
 // - Додає в боді блок з текстом "Hello owu"
 
-function inputTextBlock() {
-    let textDiv = document.createElement('div');
-    textDiv.innerText = 'Hello owu';
-    document.body.appendChild(textDiv);
-}
-inputTextBlock()
-
-// - Додає в боді елемент з текстом. Тип елементу та текст отримати через аргументи
-
-function inputTextElement(enter_str ) {
-    let elementCreate = document.createElement("p");
-    elementCreate.innerText = (enter_str);
-    document.body.appendChild(elementCreate);
-}
-inputTextElement('PIU PIU');
+// function inputTextBlock() {
+//     let textDiv = document.createElement('div');
+//     textDiv.innerText = 'Hello owu';
+//     document.body.appendChild(textDiv);
+// }
+//
+// inputTextBlock()
+//
+// // - Додає в боді елемент з текстом. Тип елементу та текст отримати через аргументи
+//
+// function inputTextElement(enter_str) {
+//     let elementCreate = document.createElement("p");
+//     elementCreate.innerText = (enter_str);
+//     document.body.appendChild(elementCreate);
+// }
+//
+// inputTextElement('PIU PIU');
 
 // - приймає масив автомобілів (можна взяти з попередніх дз ),та  індентифікатор елемнту в який
 // потрібно додати список цих автомобілів.
@@ -250,17 +278,13 @@ inputTextElement('PIU PIU');
 // Всі властивості авто в обному блоці
 
 
-
 // - приймає масив автомобілів (можна взяти з попередніх дз ),та  індентифікатор елемнту в який потрібно додати список цих автомобілів.
-
 
 
 // Для кожного автомобіля створити свій блок, та додати його в елемент, індентифікатор якого ви отримали.
 
 
-
 // Для кожної властивості створити всередені блока автомоблія свій блок
-
 
 
 // // ******************* НАЗАД В МИНУЛЕ *********************** //
